c97852ec5dac8c3fd848e9a66afb146f303398dc
Arturo Sotelo Bayangos <ambibayangos@gmail.com>
2020-07-10 16:13:06 +1200

    basic landing page layout

--- Web-Portfolio/Web-Portfolio/Front-End/src/App.tsx
+++ Web-Portfolio/Web-Portfolio/Front-End/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-   <h1>Cleaned Template</h1>
+    <div className="wrapper">
+      <div className="header">
+        <h1 className="title">EEE Portfolio</h1>
+        <div className="goto">
+          <h3 className="projects">Projects</h3>
+          <h3 className="blogs">Blogs</h3>
+          <h3 className="about">About</h3>
+        </div>
+      </div>
+      <div className="main-section">
+        <div className="body-main">Main body contents</div>
+        <div className="nav-bar">Navigation</div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
